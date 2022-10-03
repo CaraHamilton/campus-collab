f7087d51acc7df30b6e79b07e542b8d1b8da5fc2
jaredbarker <jaredbarker99@gmail.com>
2022-10-03 16:04:31 -0600

    Set hardcoded school options.  Wired layouts in the class

--- app/src/main/java/com/example/campuscollab/SchoolFragment.java
+++ app/src/main/java/com/example/campuscollab/SchoolFragment.java
@@ -1,4 +1,70 @@
 package com.example.campuscollab;
 
-public class SchoolFragment {
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.Layout;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.campuscollab.databinding.SchoolFragmentBinding;
+import com.example.campuscollab.service.SchoolService;
+
+public class SchoolFragment extends Fragment {
+    private SchoolFragmentBinding binding;
+    private SchoolService schoolService;
+    private LinearLayout byu;
+    private LinearLayout byui;
+    private LinearLayout byuh;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = SchoolFragmentBinding.inflate(inflater, container, false);
+        byu = binding.byuSchool;
+        byui = binding.byuiSchool;
+        byuh = binding.byuhSchool;
+
+        byu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        byui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        byuh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
