285bc74885edca2cae8f17ee6ea7908f232256d3
jaredbarker <jaredbarker99@gmail.com>
2022-10-05 21:14:24 -0600

    Fixed style and added navigation to register fragment with school passed as an argument

--- app/src/main/java/com/example/campuscollab/view/SchoolFragment.java
+++ app/src/main/java/com/example/campuscollab/view/SchoolFragment.java
@@ -1,5 +1,6 @@
-package com.example.campuscollab;
+package com.example.campuscollab.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Layout;
@@ -12,9 +13,11 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavAction;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.campuscollab.R;
 import com.example.campuscollab.databinding.SchoolFragmentBinding;
 import com.example.campuscollab.service.SchoolService;
 
@@ -30,7 +33,6 @@ public class SchoolFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = SchoolFragmentBinding.inflate(inflater, container, false);
         byu = binding.byuSchool;
         byui = binding.byuiSchool;
@@ -39,19 +41,28 @@ public class SchoolFragment extends Fragment {
         byu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Bundle bundle = new Bundle();
+                bundle.putString("school_name", "Brigham Young University");
+                NavHostFragment.findNavController(SchoolFragment.this)
+                        .navigate(R.id.action_schoolFragment_to_accountInformationFragment, bundle);
             }
         });
         byui.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Bundle bundle = new Bundle();
+                bundle.putString("school_name", "Brigham Young University - Idaho");
+                NavHostFragment.findNavController(SchoolFragment.this)
+                        .navigate(R.id.action_schoolFragment_to_accountInformationFragment, bundle);
             }
         });
         byuh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Bundle bundle = new Bundle();
+                bundle.putString("school_name", "Brigham Young University - Hawaii");
+                NavHostFragment.findNavController(SchoolFragment.this)
+                        .navigate(R.id.action_schoolFragment_to_accountInformationFragment, bundle);
             }
         });
         return binding.getRoot();
