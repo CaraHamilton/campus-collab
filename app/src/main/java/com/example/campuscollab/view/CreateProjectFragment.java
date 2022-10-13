44c18b27ae3e68f9d01b0f53a174195649be1310
jaredbarker <jaredbarker99@gmail.com>
2022-10-12 21:34:48 -0600

    Create initial xml layout

---
+++ app/src/main/java/com/example/campuscollab/view/CreateProjectFragment.java
@@ -0,0 +1,35 @@
+package com.example.campuscollab.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.campuscollab.databinding.FragmentCreateProjectBinding;
+
+public class CreateProjectFragment extends Fragment {
+    private FragmentCreateProjectBinding binding;
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentCreateProjectBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
