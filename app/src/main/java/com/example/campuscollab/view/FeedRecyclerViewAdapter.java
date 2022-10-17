732930d00ab25478f03d833f5c20b03ae9604815
CaraHamilton <carahami13@gmail.com>
2022-10-16 21:33:57 -0600

    creates project feed fragment

---
+++ app/src/main/java/com/example/campuscollab/view/FeedRecyclerViewAdapter.java
@@ -0,0 +1,50 @@
+package com.example.campuscollab.view;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.campuscollab.domain.Project;
+import com.example.campuscollab.databinding.FragmentFeedBinding;
+
+import java.util.List;
+
+public class FeedRecyclerViewAdapter extends RecyclerView.Adapter<FeedRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Project> feedProjects;
+
+    public FeedRecyclerViewAdapter(List<Project> items) {
+        feedProjects = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentFeedBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.title.setText(feedProjects.get(position).getProjectName());
+        holder.description.setText(feedProjects.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return feedProjects.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView description;
+
+        public ViewHolder(FragmentFeedBinding binding) {
+            super(binding.getRoot());
+            title = binding.title;
+            description = binding.description;
+        }
+    }
+}
